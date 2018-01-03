ddc39e64351b5e97777c22514909d8a9bd561d52
Suman Kumar Jha <jhasumankumar@rediffmail.com>
2018-01-03 13:47:48 +0100

    Add files via upload
    First commit for zookeeper  Curator framework to register services with zookeeper

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# zookeeperServiceDiscovery
- Curator framework to register services with zookeeper
+#    Sbring Boot Zookeepr client.
+
+### It uses
+    Zookeeper 3.7
+    Spring Boot 1.5.4.RELEASE
+    Java 8
+    Curator framework to register services with zookeeper
+
+
+This application connects to Zookeeper server usring rest paths application can be registered/discovered in cloud environment.
+Zookeeper should be up and running for this application to register.
+
+# Test
+Use rest client to test .
+
+
+- GET method - http://localhost:8081/findAll - list all services registered
+   
+     [
+         "Zookeeper_Service",
+         "suman"
+     ]
+- PUT method - http://localhost:8081/create/localhost/82881/suman (add service named suman running on port 82881 with URI http://localhost:82881)
+    {
+        "serviceId": "suman",
+        "host": "localhost",
+        "port": 82882,
+        "secure": false,
+        "uri": "http://localhost:82882",
+        "metadata": {}
+    }
+- GET method http://localhost:8081/find/suman find service information for service suman
+
+   {
+       "serviceId": "suman",
+       "host": "localhost",
+       "port": 82881,
+       "secure": false,
+       "uri": "http://localhost:82881",
+       "metadata": {}
+   }
---
+++ src/main/java/com/sj/zService/controller/ZServiceController.java
@@ -0,0 +1,71 @@
+package com.sj.zService.controller;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.zookeeper.serviceregistry.ServiceInstanceRegistration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
+import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperServiceRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Controller
+@RestController
+public class ZServiceController {
+
+    @Autowired
+    private DiscoveryClient discovery;
+    @Autowired
+    private ZookeeperServiceRegistry serviceRegistry;
+
+    @RequestMapping(value = "/create/{address}/{port}/{zName}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<List<ServiceInstance>> createZNode(@PathVariable final String address,@PathVariable final Integer port, @PathVariable final String zName) {
+        ZookeeperRegistration registration = ServiceInstanceRegistration.builder()
+                .defaultUriSpec()
+                .address(address)
+                .port(port)
+                .name(zName)
+                .build();
+        this.serviceRegistry.register(registration);
+        return new ResponseEntity(discovery.getInstances(zName), HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/find/{zName}", method = RequestMethod.GET)
+    public ResponseEntity<ServiceInstance> findByServiceName(@PathVariable final String zName) {
+        List<ServiceInstance> serviceInstances = discovery.getInstances(zName);
+        if(CollectionUtils.isEmpty(serviceInstances)){
+            return new ResponseEntity("No ZNode found for zName " + zName, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(discovery.getInstances(zName).get(0), HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/findAll")
+    public ResponseEntity<List<String>> findAllServices() {
+        return new ResponseEntity(discovery.getServices(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/delete/{zName}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity delete(@PathVariable final String zName) {
+        List<ServiceInstance> serviceInstances = discovery.getInstances(zName);
+        if(CollectionUtils.isEmpty(serviceInstances)){
+            return new ResponseEntity("No ZNode found for zName " + zName, HttpStatus.NOT_FOUND);
+        }
+        ServiceInstance instance =serviceInstances.get(0);
+        ZookeeperRegistration registration = ServiceInstanceRegistration.builder()
+                .defaultUriSpec()
+                .address(instance.getHost())
+                .port(instance.getPort())
+                .name(instance.getServiceId())
+                .build();
+        this.serviceRegistry.deregister(registration);
+        return new ResponseEntity(zName, HttpStatus.OK);
+    }
+}
